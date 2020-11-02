6618f14588f478384c43e431591337aef48a535f
Komal <komalbansal97@gmail.com>
2020-11-02 19:39:43 +0530

    updated date time format of popup and badge

--- scripts/common.js
+++ scripts/common.js
@@ -6,7 +6,7 @@ sort object based on multiple key properties
 */
 const sortMultipleProperties = (...property) => {
 
-    return function (obj1, obj2) {
+    return function(obj1, obj2) {
         let i = 0,
             result = 0,
             numberOfProperties = property.length;
@@ -30,7 +30,7 @@ const sortData = (property) => {
         property = property.substr(1);
     }
 
-    return function (a, b) {
+    return function(a, b) {
         let result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
         return result * sortOrder;
     }
@@ -41,6 +41,9 @@ format time in 00h 00m 00s
 */
 const formatTime = (time) => {
 
+    let days = Math.floor(time / (60 * 60 * 24));
+    time -= days * (60 * 60 * 24);
+
     let hours = Math.floor(time / 3600);
     time -= hours * 3600;
 
@@ -49,21 +52,22 @@ const formatTime = (time) => {
 
     let seconds = parseInt(time % 60, 10);
 
-    let formatedTime = (hours === 0 || hours < 10 ? "0" + hours : hours) + "h " +
+    let formatedTime = (days === 0 || days < 10 ? "0" + days : days) + "d " +
+        (hours === 0 || hours < 10 ? "0" + hours : hours) + "h " +
         (minutes < 10 ? "0" + minutes : minutes) + "m " +
         (seconds < 10 ? "0" + seconds : seconds) + "s";
     return formatedTime;
-
 }
 
 /*
 convert time to format display on icon badge
 */
 const convertTimeToBadgeString = (time) => {
-    var sec = (time);
-    var min = (time / 60).toFixed(0);
-    var hours = (time / (60 * 60)).toFixed(0);
-    var days = (time / (60 * 60 * 24)).toFixed(0);
+
+    let sec = (time);
+    let min = Math.floor(time / 60);
+    let hours = Math.floor(time / (60 * 60));
+    let days = Math.floor(time / (60 * 60 * 24));
 
     if (sec < 60) {
         return sec + "s";
